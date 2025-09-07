41b87608157aefcd329c25c2404d615a7a58529f
Watricktoo <Watricktwo@gmail.com>
2025-09-07 13:33:16 -0500

    Progress on calc wages

--- shipFunds.py
+++ shipFunds.py
@@ -1,6 +1,40 @@
 ## Calculator for Ship Funds
 
 #Global Costs Variables
-ship_Passage = .5
-messages_Dock_Master = 2
-messages_Addressed = 5
+#*ship_Passage = .5
+#messages_Dock_Master = 2
+#messages_Addressed = 5
+#docking_cost = 10
+
+#Wages for each member
+wages = {
+    'Sailors' : 0.2,
+    'Skilled Sailors' : 0.5,
+    'Marines' : 1,
+    'Officers' : 2,
+    'Wizards' : 2,
+}
+
+#Total Money on ship
+shipFundTotal = 0
+daysOnTrip = 0
+
+#Adding money to the ship total
+def addMoneyToShip(money):
+    shipFundTotal += money
+    return shipFundTotal
+
+#Adding days to journey, for wages
+def addDaysOnTrip(days):
+    daysOnTrip += days
+    return daysOnTrip
+
+def wagesForCrew(daysOnTrip, wages):
+    
+
+    
+
+#Calculating trip wages
+
+    
+    

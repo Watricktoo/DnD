3e2bca60deda285a1243059c985108e3e31ef6f0
Watricktoo <Watricktwo@gmail.com>
2025-09-07 22:16:45 -0500

    added comments to the code for the code

--- shipFunds.py
+++ shipFunds.py
@@ -24,7 +24,7 @@ crew = {
 }
 #Total Money on ship
 shipFundTotal = 0
-daysOnTrip = 0
+daysOnTrip = 1
 
 #Adding money to the ship total
 def addMoneyToShip(money):
@@ -40,21 +40,31 @@ def addDaysOnTrip(days):
 
 #Calculating trip wages
 def calculateCrewWages(days, wages, crew):
+    #Calc the cost of a single crew member of each type
     costOfSailor = days * wages['Sailors']
     costOfSkilledSailor = days * wages['Skilled Sailors']
     costOfMarine = days * wages['Marines']
     costOfOfficer= days * wages['Officers']
     costOfWizard = days * wages['Wizards']
 
+    #Each player is a officer or wizard so mention amount of gold to players
     print(f"Each Officer gets {costOfOfficer} gold and each Wizard gets {costOfWizard} gold")
 
+    #Calc the total cost of each crew member type for the total number of crew
     costOfSailors = costOfSailor * crew['Sailors']
     costOfSkillerSailors = costOfSkilledSailor * crew['Skilled Sailors']
     costOfMarines = costOfMarine * crew["Marines"]
     costOfOfficers = costOfOfficer * crew["Officers"]
     costOfWizards = costOfWizard * crew["Wizards"]
 
+    #Sum the total of cost & print/return total
     totalCrewWages = costOfSailors + costOfSkillerSailors + costOfMarines + costOfOfficers + costOfWizards
-    print(f"The crew cost a total of {totalCrewWages} in wages for this voyage")
+    print(f"The crew cost a total of {totalCrewWages} gold in wages for this voyage")
     return totalCrewWages
-totalCrewWages = calculateCrewWages(1, wages, crew)
+
+#Collecting value of Crew Wages
+totalCrewWages = calculateCrewWages(daysOnTrip, wages, crew)
+
+#Subtracting Crew wages from Funds and print out remain Funds
+shipFundTotal -= totalCrewWages
+print(f"There is {shipFundTotal} gold in Ship Funds remaining after wages")
